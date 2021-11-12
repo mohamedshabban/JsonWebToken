9bc5644741fe7807abeeaddc3a6c8c2208117f8c
MoShaban <MoShaban@DESKTOP-6MUK6L7>
2021-11-12 14:28:35 +0200

    Add project files.

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+namespace JWT.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class BaseController : ControllerBase
+    {
+
+    }
+}
